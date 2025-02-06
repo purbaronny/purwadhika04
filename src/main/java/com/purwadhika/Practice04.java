7ed56c47d0086869ce550f70afb67a715f7745b5
purbaronny <95927137+purbaronny@users.noreply.github.com>
2025-02-06 13:22:20 +0700

    feat(printStar) initial

--- src/main/java/com/purwadhika/Practice04.java
+++ src/main/java/com/purwadhika/Practice04.java
@@ -1,4 +1,37 @@
 package com.purwadhika;
 
 public class Practice04 {
+
+    public void printMultiplicationTable(int n) {
+        if(n < 1) {
+            throw new IllegalArgumentException("n must be more than 0");
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(i * (j+ 1));
+                if(j < n - 1) {
+                    System.out.print(" ");
+                }
+            }
+            if(i < n) {
+                System.out.println();
+            }
+        }
+    }
+
+    public void printStar(int n) {
+        if(n < 1) {
+            throw new IllegalArgumentException("n must be more than 0");
+        }
+
+        // TODO start
+        // TODO end
+    }
+
+    public static void main(String[] args) {
+        Practice04 practice04 = new Practice04();
+        System.out.println("Create a program that prints a multiplication table up to n x n.");
+        practice04.printMultiplicationTable(5);
+    }
 }
